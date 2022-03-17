2018e1a77b0c83619d68a41c57d664b1f8e18a47
Mohammed Musthafa <musthafa.m1996@gmail.com>
2022-03-17 21:39:28 +0530

    RC 2.1.0+90

--- lib/pages/patient_search_page.dart
+++ lib/pages/patient_search_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:hokmabadi/utils/snackbar_messenger.dart';
 
